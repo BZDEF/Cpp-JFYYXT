9dd8be05b9ead83d923321640ccaec33db4a3e90
Kavin <96652556+BZDEF@users.noreply.github.com>
2022-08-19 15:09:51 +0800

    Add files via upload

---
+++ globalFile.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+//管理员文件
+#define ADMIN_FILE     "admin.txt"
+//学生文件
+#define STUDENT_FILE   "student.txt"
+//教师文件
+#define TEACHER_FILE   "teacher.txt"
+//机房信息文件
+#define COMPUTER_FILE  "computerRoom.txt"
+//订单文件
+#define ORDER_FILE     "order.txt"
